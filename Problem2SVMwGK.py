07b0a48c91e7563422e3fc35d5348ec520066c58
ChaseRensberger <chasehrensberger@gmail.com>
2022-11-11 19:52:34 -0500

    classifications done?

--- Problem2SVMwGK.py
+++ Problem2SVMwGK.py
@@ -2,7 +2,7 @@
 # The hyper paramaters we are modifying are:
 # kernel_type Default: 'rbf'
 # gamma Default: 'scale'
-# c Default: '1.0'
+# c Default: 1.0
 
 
 from sklearn.datasets import load_svmlight_file
@@ -27,24 +27,24 @@ def determine_SVM_hp(X_train, y_train, X_test, y_test, default_kernel_type, defa
     default_score = metrics.accuracy_score(y_test, rf_default_prediction)
     print("Default score: " + str(default_score))
 
-    # param_grid = {
-    #     'n_estimators': [x for x in range(100, 1000, 50)],
-    #     'bootstrap': [True, False],
-    #     'max_depth': [None, 5, 10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
-    # }
+    param_grid = {
+        'kernel': ['rbf', 'linear', 'poly', 'sigmoid'],
+        'gamma': ['scale', 'auto'],
+        'C': [0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0],
+    }
 
-    # rf = RandomForestClassifier()
-    # random_search = RandomizedSearchCV(estimator = rf, param_distributions=param_grid, cv = 5, n_jobs = -1, verbose = 2, return_train_score=True, n_iter=20)
-    # random_search.fit(X_train, y_train)
-    # rf_best_params = random_search.best_params_
+    rf = SVC()
+    random_search = RandomizedSearchCV(estimator = rf, param_distributions=param_grid, cv = 5, n_jobs = -1, verbose = 2, return_train_score=True, n_iter=20)
+    random_search.fit(X_train, y_train)
+    rf_best_params = random_search.best_params_
 
-    # rf_best_params_classifier = RandomForestClassifier(n_estimators=rf_best_params['n_estimators'], bootstrap=rf_best_params['bootstrap'], max_depth=rf_best_params['max_depth'], min_impurity_decrease=rf_best_params['min_impurity_decrease'], min_samples_leaf=rf_best_params['min_samples_leaf'])
-    # rf_best_params_classifier.fit(X_train, y_train)
-    # rf_best_params_prediction = rf_best_params_classifier.predict(X_test)
-    # best_params_score = metrics.accuracy_score(y_test, rf_best_params_prediction)
-    # print("Best parameters score: " + str(best_params_score))
+    rf_best_params_classifier = rf_default = SVC(kernel=rf_best_params['kernel'], gamma=rf_best_params['gamma'], C=rf_best_params['C'])
+    rf_best_params_classifier.fit(X_train, y_train)
+    rf_best_params_prediction = rf_best_params_classifier.predict(X_test)
+    best_params_score = metrics.accuracy_score(y_test, rf_best_params_prediction)
+    print("Best parameters score: " + str(best_params_score))
 
-    # return (default_score, rf_best_params, best_params_score, random_search.cv_results_)
+    return (default_score, rf_best_params, best_params_score, random_search.cv_results_)
     
 
 
